2dd297d2bb3c968d2121af424938a6614b7cd16a
Matias Arrech <matias.arrech@gba.gob.ar>
2021-03-11 18:11:05 -0300

    Added crop and resize square

--- components/utils/cropAndResize.js
+++ components/utils/cropAndResize.js
@@ -74,8 +74,8 @@ export function cropAndResizeForDetector(
   inputTensorHeight,
   topLeft,
   bottomRight,
-  displacementTop = 0.0029,
-  displacementBottom = 0.0025,
+  displacementTop = 0.002,
+  displacementBottom = 0.001,
 ) {
   const boxHeight = ((bottomRight[1] - topLeft[1]) * 100) / inputTensorHeight;
 
@@ -91,6 +91,36 @@ export function cropAndResizeForDetector(
   const cropWidth = Math.floor((x2 - x1) * (inputTensorWidth - 1));
 
   console.log(cropHeight, cropWidth);
+
+  return tf.image
+    .cropAndResize(
+      imageTensor.reshape([1, inputTensorHeight, inputTensorWidth, 3]),
+      [[y1, x1, y2, x2]],
+      [0],
+      [IMAGE_SIZE, IMAGE_SIZE],
+    )
+    .reshape([IMAGE_SIZE, IMAGE_SIZE, 3]);
+}
+
+export function cropAndResizeForDetectorSquare(
+  imageTensor,
+  inputTensorWidth,
+  inputTensorHeight,
+  topLeft,
+  bottomRight,
+  growRatio = 0.1,
+  displacement = 0.0028,
+) {
+  const boxWidthRaw = (bottomRight[0] - topLeft[0]) * (1 + growRatio * 2);
+  const boxHeight = ((bottomRight[1] - topLeft[1]) * 100) / inputTensorHeight;
+
+  const y1 = topLeft[1] / inputTensorHeight + boxHeight * displacement;
+
+  const y2 =
+    (topLeft[1] + boxWidthRaw) / inputTensorHeight + boxHeight * displacement;
+
+  const x1 = (topLeft[0] - topLeft[0] * growRatio) / inputTensorWidth;
+  const x2 = (bottomRight[0] + bottomRight[0] * growRatio) / inputTensorWidth;
 
   return tf.image
     .cropAndResize(
