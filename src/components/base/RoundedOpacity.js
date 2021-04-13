65c6d78e53d79ffbe8cd1c0581a35a4c9bdc62d7
daniffig <daniffig@gmail.com>
2021-04-13 01:57:56 -0300

    added random backgrounds

--- src/components/base/RoundedOpacity.js
+++ src/components/base/RoundedOpacity.js
@@ -3,6 +3,11 @@ import {Image, StyleSheet, TouchableOpacity} from 'react-native';
 import {Text} from '@ui-kitten/components';
 
 export default class RoundedOpacity extends React.Component {
+  renderIcon() {
+    if (this.props.icon) {
+      return <Image style={styles.icon} source={this.props.icon} />;
+    }
+  }
   renderText() {
     if (this.props.text) {
       return <Text style={styles.text}>{this.props.text}</Text>;
@@ -13,7 +18,7 @@ export default class RoundedOpacity extends React.Component {
       <TouchableOpacity
         style={styles.touchable}
         onPress={() => this.props.action()}>
-        <Image style={styles.icon} source={this.props.icon} />
+        {this.renderIcon()}
         {this.renderText()}
       </TouchableOpacity>
     );
@@ -23,7 +28,7 @@ export default class RoundedOpacity extends React.Component {
 const styles = StyleSheet.create({
   touchable: {
     alignItems: 'center',
-    backgroundColor: 'rgba(100, 200, 200, 0.6)',
+    backgroundColor: 'rgba(100, 200, 200, 0.4)',
     borderRadius: 120,
     height: 240,
     justifyContent: 'center',
