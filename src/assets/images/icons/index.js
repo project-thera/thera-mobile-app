c234085ba641ee9cf7cf612cea16e86c091033c5
daniffig <daniffig@gmail.com>
2021-04-25 04:46:47 -0300

    added cool stuff to workshop

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,4 +1,7 @@
 export default icons = {
+  cogwheelRobot: require('./cogwheel-robot.png'),
+  microprocessor: require('./microprocessor.png'),
   projectTheraIcon: require('./project-thera-icon.png'),
   profTheralovIcon: require('./prof-theralov-icon.png'),
+  robot: require('./robot.png'),
 };
